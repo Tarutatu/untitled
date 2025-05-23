f1157377a88b8323b5c40b29890d0158861f046d
TaniHinano <241050@std.hi-joho.ac.jp>
2025-05-23 11:27:36 +0900

    Review4　追加

---
+++ src/kadai/Review4.java
@@ -0,0 +1,16 @@
+package kadai;
+
+public class Review4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String str = "88";
+        final double PI = 3.141592;
+        final double pi = 3.14;
+        double circ = 3.14;
+        long a = 1000000000;
+        boolean flg = true;
+        byte b = 35;
+        int num = 0x735;
+        String c = "a";
+    }
+}
