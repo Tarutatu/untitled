cefa7d3352c4e40bf7e31f5f1e824ff14b0c5379
TaniHinano <241050@std.hi-joho.ac.jp>
2025-06-06 12:00:45 +0900

    Review17 追加

--- src/reviews/reviews2/Review17.java
+++ src/reviews/reviews2/Review17.java
@@ -4,8 +4,10 @@ public class Review17 {
     public static void main(String[] args) {
         int num = 8;
         int sum = 0;
-        for (int i = 2; i <= num; i+=2){
+        int i = 2;
+        while ( i <= num){
             sum = sum + i;
+            i += 2;
         }
         System.out.println("結果: "+sum);
     }
