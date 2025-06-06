0814cee4f12c14dddae6093393eda356e83a2a8a
TaniHinano <241050@std.hi-joho.ac.jp>
2025-06-06 12:01:31 +0900

    Review21 追加

---
+++ src/reviews/reviews2/Review21.java
@@ -0,0 +1,12 @@
+package reviews.reviews2;
+
+public class Review21 {
+    public static void main(String[] args) {
+        int num = 4;
+        int sum = 0;
+        for(int i = 1; i <=num; i += 2) {
+            sum += i;
+        }
+        System.out.println("結果："+sum);
+    }
+}
